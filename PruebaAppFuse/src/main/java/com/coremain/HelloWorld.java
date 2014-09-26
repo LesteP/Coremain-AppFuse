e8f811c527b4bf276d4dd5001a22cc2b926e361b
Luis Esteve <luis.esteve.coremain@gmail.com>
2014-09-26 14:04:02 +0200

    Hello World

---
+++ PruebaAppFuse/src/main/java/com/coremain/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.coremain;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this is Java 7!!!");
+
+	}
+
+}
