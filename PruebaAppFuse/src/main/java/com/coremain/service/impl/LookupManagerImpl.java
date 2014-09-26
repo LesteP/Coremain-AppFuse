714523ef6dd8643d0bd92b6a2e4a6d86bb94b90e
Luis Esteve <luis.esteve.coremain@gmail.com>
2014-09-26 13:57:59 +0200

    Primera subida - Project BBDD Coremain

---
+++ PruebaAppFuse/src/main/java/com/coremain/service/impl/LookupManagerImpl.java
@@ -0,0 +1,37 @@
+package com.coremain.service.impl;
+
+import com.coremain.dao.LookupDao;
+import com.coremain.model.LabelValue;
+import com.coremain.model.Role;
+import com.coremain.service.LookupManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Implementation of LookupManager interface to talk to the persistence layer.
+ *
+ * @author <a href="mailto:matt@raibledesigns.com">Matt Raible</a>
+ */
+@Service("lookupManager")
+public class LookupManagerImpl implements LookupManager {
+    @Autowired
+    LookupDao dao;
+
+    /**
+     * {@inheritDoc}
+     */
+    public List<LabelValue> getAllRoles() {
+        List<Role> roles = dao.getRoles();
+        List<LabelValue> list = new ArrayList<LabelValue>();
+
+        for (Role role1 : roles) {
+            list.add(new LabelValue(role1.getName(), role1.getName()));
+        }
+
+        return list;
+    }
+}
